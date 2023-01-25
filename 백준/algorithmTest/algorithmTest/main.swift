41d90cd18c514f785b517629d264ddda523037b7
SEO-YJ <tjdbwns19@naver.com>
2023-01-25 17:01:19 +0900

    [add] 1330

---
+++ 백준/algorithmTest/algorithmTest/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  algorithmTest
+//
+//  Created by 서유준 on 2023/01/24.
+//
+
+import Foundation
+
+//MARK: 1008
+//let aArr = readLine()!.split(separator: " ").map{ Double($0)! }
+//print(aArr[0]/aArr[1])
+
+//MARK: 1330
+//let aArr = readLine()!.components(separatedBy: " ")
+//
+//if aArr[0] > aArr[1] {
+//    print(">")
+//} else if aArr[0] < aArr[1] {
+//    print("<")
+//} else {
+//    print("==")
+//}
