7708b8ca200906162bc8a1e6398b9484f366ee1b
SEO-YJ <tjdbwns19@naver.com>
2023-01-31 16:03:36 +0900

    [add] 11654

--- 백준/algorithmTest/algorithmTest/main.swift
+++ 백준/algorithmTest/algorithmTest/main.swift
@@ -98,3 +98,6 @@ import Foundation
 //while let input = readLine() {
 //    print(input.split(separator: " ").map{ Int($0)! }.reduce(0){ $0 + $1 })
 //}
+
+//MARK: 11654
+//print(Character(readLine()!).asciiValue)
